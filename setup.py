0ae6c1d92f7f72ccdd5cf57f062eec56f449c77a
Eder de Souza <ederwander@gmail.com>
2020-07-29 20:24:20 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,14 @@
 from distutils.core import setup, Extension
+import numpy
  
 Tuner = Extension('AutoTune', ['PyAutoTuner.c', 'autotalent.c', 'mayer_fft.c'])
  
 
-setup (name = 'PackageName',
+setup (name = 'PyAutoTune',
         version = '0.1b',
         author = "Eng Eder de Souza",
         author_email = "ederwander@gmail.com",
         url = "http://github.com/ederwander/PyAutoTune",
         description = 'AutoTune package',
-        ext_modules = [Tuner])
+        ext_modules = [Tuner],
+        include_dirs = [numpy.get_include()])
